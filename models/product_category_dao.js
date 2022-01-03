78210865d1a1a446b4725e5d0c6133e6f0675b63
박효상 <45405912+pd0203@users.noreply.github.com>
2022-01-03 16:21:37 +0900

    Edit: merge error resolved

--- models/product_category_dao.js
+++ models/product_category_dao.js
@@ -28,29 +28,5 @@ const getSubCategories = async () => {
   return category;
 };
 
-<<<<<<< HEAD
-const findCategories = async (category_id, sub_category_id) => {
-  const category = await prisma.$queryRaw`
-  SELECT
-    categories.id As categoryId,
-    categories.name As category,
-    sub_categories.id As subCategoryId,
-    sub_categories.name AS subCategory
-  FROM
-    categories
-  JOIN
-    sub_categories
-  ON
-    categories.id = sub_categories.category_id
-  WHERE
-    categories.id = ${category_id} 
-  AND 
-    sub_categories.id = ${sub_category_id}
-  `;
-  return category;
-};
-
-module.exports = { getCategories, getSubCategories, findCategories };
-=======
 module.exports = { getCategories, getSubCategories };
->>>>>>> develop
+
