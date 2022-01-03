d2a221bfc357006568a4078b0bc31d724a146dfa
woongstaa <jinung91@gmail.com>
2022-01-03 12:38:23 +0900

    Edit: fix some issues

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const productRouter = require('./product_router');
+const categoryRouter = require('./category_router');
 
 router.use('/products', productRouter);
+router.use('/category', categoryRouter);
 
 module.exports = router;
--- routes/product_router.js
+++ routes/product_router.js
@@ -4,15 +4,10 @@ const router = express.Router();
 const {
   productDetailController,
   productSortController,
-  productCategoryController,
 } = require('../controllers');
 
 router.get('/details', productDetailController.productDetail);
 
 router.get('/sort', productSortController.productSort);
-
-router.get('/category', productCategoryController.categories);
-router.get('/sub_category', productCategoryController.subCategories);
-router.post('/filter_category', productCategoryController.filterCategories);
 
 module.exports = router;
