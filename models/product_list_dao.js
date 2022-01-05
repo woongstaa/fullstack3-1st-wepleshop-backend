b2e1b385c60c392c4b0f953cfc8a32e1c8ee7031
woongstaa <jinung91@gmail.com>
2022-01-05 14:45:58 +0900

    Edit: product_list_dao

--- models/product_list_dao.js
+++ models/product_list_dao.js
@@ -9,9 +9,18 @@ const productList = async (categoryId, subCategoryId, sortOption) => {
       products.name as productName,
       products.price,
       products.like_count as likeCount,
-      product_imgs.img_url as imgUrl
+      product_imgs.img_url as imgUrl,
+      categories.name as categoryName,
+      sub_categories.name as subCategoryName,
+      product_details.quantity
     FROM
       products
+    JOIN
+      product_details ON products.id = product_details.product_id
+    JOIN
+      categories ON products.category_id = categories.id
+    JOIN
+      sub_categories ON products.subcategory_id = sub_categories.id
     JOIN
       product_imgs ON products.id = product_imgs.product_id
     WHERE
